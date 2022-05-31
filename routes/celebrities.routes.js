7943c0f36965ad35ea4955b13a38b28c6b2c6d14
Lucas Pfister <digitalservices@outlook.de>
2022-05-31 12:18:46 +0200

    task 4 done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,5 +1,28 @@
 const router = require('express').Router()
 
-// all your routes here
+const Celebrity = require('../models/Celebrity.model')
+
+router.get("/celebrities", async (req, res, next) => {
+   const allCelebs = await Celebrity.find()
+   //console.log(allCelebs)
+   res.render("../views/celebrities/celebrities.ejs", {allCelebs})
+})
+
+router.get("/celebrities/create", (req, res, next) => {
+    console.log("ddddd")
+    res.render("../views/celebrities/new-celebrities")
+})
+
+router.post("/celebrities/create", async (req, res, next) => {
+    try {
+       const newCelebrity = await Celebrity.create(req.body)
+       console.log(req.body)
+       res.redirect("/celebrities/")
+   } catch (err) {
+        res.redirect("/celebrities/create");
+        console.log("this POST-method is just not working!!", (err))
+   }
+})
+
 
 module.exports = router
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,4 +1,3 @@
-// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require('express').Router()
 
 // all your routes here
